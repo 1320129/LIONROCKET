1d23af6ff2b794765bda261e15ba0f868200c6f9
노재혁 <jaehyuck.noh@pxd.co.kr>
2025-09-10 12:11:25 +0900

    style: MessageItem.tsx import 포맷팅 개선
    - import 문을 여러 줄로 정리하여 가독성 향상
    - ESLint 규칙에 따른 일관된 포맷팅 적용

--- frontend/src/components/MessageItem.tsx
+++ frontend/src/components/MessageItem.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { MessageContainer, MessageMeta, RetryButton, MutedText } from "../ui/styled";
+import {
+  MessageContainer,
+  MessageMeta,
+  RetryButton,
+  MutedText,
+} from "../ui/styled";
 import { formatMessageTime } from "../utils/chatUtils";
 import { MessageWithStatus } from "../types/message";
 
